a2971d36c547dd5e37fb1db6ad7d4bba311377d9
Xiaoshan0509 <40324102@gm.nfu.edu.tw>
2021-09-15 23:00:55 +0800

    0915筱姍commit01

--- src/main/java/tw/paintingparty/util/Jdbc.java
+++ src/main/java/tw/paintingparty/util/Jdbc.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class Jdbc {
 	public Connection Jdbc_connection() {
 		String connectionUrl = "jdbc:sqlserver://localhost:1433;databasename=paintingparty";
-		String user="sa";
-		String password="as";
+		String user="haha";
+		String password="manager";
 		Connection con=null;
 		try {
 			con = DriverManager.getConnection(connectionUrl,user,password);
